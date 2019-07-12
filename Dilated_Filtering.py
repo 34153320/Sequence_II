820011515c2a1a1f0e99c07c7163533a84961b74
Pengfei Sun <sunpengfei1985@gmail.com>
2019-07-12 15:05:21 -0700

    modify the astrouds conv to general

--- Dilated_Filtering.py
+++ Dilated_Filtering.py
@@ -21,7 +21,7 @@ class DFiltering(object):
           dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
           with tf.variable_scope(scope):
                shape  = input_tensor.get_shape().as_list()
-               kernel_1 = tf.get_variable('kernel_1',
+               kernel_1 = tf.get_tf.nn.atrous_conv2dvariable('kernel_1',
                               (size, shape[-1], dim), # input channel
                               initializer=tf.contrib.layers.xavier_initializer(),
                               dtype=tf.float32)
@@ -97,7 +97,7 @@ class DFiltering(object):
                    return layer_output 
                
 Class OctDilating(object):
-      """ Octave dilation CNN.
+      """ Octave dilated CNN.
           The model is proposed to overcome the deficit of spectral bias of neural networks. 
           Dilated CNN is applied to obtain different frequecy band information, but limited by 
           neural network are easily over-emphsizing the low frequency components. Octave operation will 
@@ -107,6 +107,65 @@ Class OctDilating(object):
          self.n_filters  = n_filters
          self.is_training=is_training
          self.kernel_size=kernel_size
+      
+       # # include the dilateion and dialted cnn
+#       def conv1d(self, input_tensor, size=1, dim=128, bias=False, activation="tanh", scope=None):
+#           dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+#           with tf.variable_scope(scope):
+#                shape  = input_tensor.get_shape().as_list()
+#                kernel_1 = tf.get_variable('kernel_1', (size, shape[-1], dim), # input channel
+#                               initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32)
+#                if bias:
+#                     b_1 = variable_on_cpu('b_1', [dim], tf.constant_initializer(0.0))
+#                out  = tf.nn.conv1d(input_tensor, kernel_1, stride=1, padding="SAME") + \
+#                                                               (b_1 if bias else 0)
+#           return out
+#             out = activation_wrapper(out, activation) 
+
+      def act_wrapper(self, input_tensor, act='tanh'):
+          """ Activation wrapper
+          """
+          if act=='tanh':
+              output_ = tf.nn.tanh(input_tensor)
+          elif act='sig':
+              output_ = tf.nn.sigmoid(input_tensor)
+          elif act='relu':
+              output_ = tf.nn.relu(input_tensor)
+          elif act='crelu':
+              output_ = tf.nn.crelu(input_tensor) # elu and other activation funcs are not listed here
+          elif act=None:
+              output_ = input_tensor
+               
+          return output_    
+      
+      def conv1d(self, input_tensor, num_filter, kernel, stride=1, rate=1, pad='valid', name=None,
+                 bias=False, w_init=None, b=None, attr=None, groups=1):
+          # basic cnn module including dilated cnn
+          if w_init is None:
+              conv_ = tf.layers.conv1d(inputs=input_tensor, filters=num_filter, kernel_size=kernel,
+                                       strides=stride, padding=pad, dilation_rate=rate, name=('%s_conv'%name), use_bias=bias)
+          else:
+              if b is None:
+                 conv_ = tf.layers.conv1d(inputs=input_tensor, filters=num_filter, kernel_size=kernel,
+                                        strides=stride, padding=pad, dilation_rate=rate, name=('%s_conv'%name), use_bias=bias,
+                                        kernel_initializer=w_init)
+              else:
+                 conv_ = tf.layers.conv1d(inputs=input_tensor, filters=num_filter, kernel_size=kernel,
+                                         strides=stride, padding=pad, dilation_rate=rate, name=('%s_conv'%name), use_bias=bias,
+                                         kernel_initializer=w_init)
+          return conv_
+      
+      # common functions for CVPR:
+      def conv_BA(self, input_tensor, num_filter, kernel, stride=1, rate=1, name=None,
+                   bias=False, w_init=None, b=None, attr=None, groups=1, act="tanh"):
+          """Conv module with batch_normalization and activation
+          """
+          conv_ = self.conv1d(input_tensor, num_filter, kernel, stride=stride, rate=rate, name=name, 
+                             bias=bias, w_init=w_init, b=b, attr=attr)
+          conv_b= tf.layers.batch_normalization(conv_, momentum=0.9, name='%s_bn' % name)
+          conv_a= self.act_wrapper(conv_b, act=act)
+          return conv_a
+ 
          
       def inputConv(self, inputs, config, in_channel, out_channel, kernel, pad, strides):
           alpha_in, alpha_out = config
@@ -116,7 +175,7 @@ Class OctDilating(object):
           hf_data  = inputs
           hf_conv  = Con1D(hf_data, filters=hf_out_channel, kernel_size=kernel, strides=1) # one dimension
           # avg_pool to yield the low frequency component
-          hf_pool  = tf.Avgpool(hf_data, kernel_size=kernel, strides=2, padding='valid')
+          hf_pool  = tf.avg_pool(hf_data, kernel_size=kernel, strides=2, padding='valid')
           lf_conv  = Con1D(hf_pool, filters=lf_out_channel, kernel_size=kernel, strides=1)
          
           self.hf_conv, self.lf_conv = hf_conv, lf_conv
@@ -131,7 +190,7 @@ Class OctDilating(object):
             
            self.out_h = hf_conv + lf_conv
          
-       def Ocvconv(self, hf_data, lf_data, config, in_channel, out_channel, kernel, stride):
+       def Octconv(self, hf_data, lf_data, config, in_channel, out_channel, kernel, stride):
            alpha_in, alpha_out = config
            hf_in_channel, hf_out_channel = int(in_channel * (1-alpha_in)), int(out_channel *(1-alpha_out))
 
@@ -156,8 +215,23 @@ Class OctDilating(object):
             
             self.out_h, self.out_l = out_h, out_l  
        
-       def  
-       
+       def oct_residual(self, input_data, ):
+           """Dilation residual with ocatave cnn
+           """
+           with tf.variable_scope("input_layer"):
+               
+                   layer_input = self.conv1d_layer(self.input_tensor, dim=nfilters,
+                                     is_training=self.is_training, scope="conv_in")
+           with tf.variable_scope("oct_layer")
+                   layer_output= 0
+                   for i in range(self.num_blocks):
+                        for i in [1, 2, 4, 8, 16]:  # dilated scope
+                             layer_input, layer_output = residual_block(layer_input, layer_output, size=11, \
+                                             rate=r, block=i, dim=FLAGS.rnn_size, \
+                                             is_training=is_training)
+           with tf.variable_scope("output_layer"):
+                
+                   return layer_output 
                
             
          
